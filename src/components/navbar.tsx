7229d70b85f17de86184b4877af2b9c52b0d385e
tossivahva <tossivahva@gmail.com>
2024-05-09 21:43:16 +0300

    implemented base for movile nav animation with framer motion

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,23 +1,23 @@
 import "@/styles/globals.css";
-import { motion, useCycle } from "framer-motion";
+import { AnimatePresence, motion, useCycle, MotionConfig } from "framer-motion";
 
 const NavItems = [
-    {
-        name: "Home",
-        link: "/",
-    },
-    {
-        name: "About",
-        link: "/about",
-    },
-    {
-        name: "Services",
-        link: "/services",
-    },
-    {
-        name: "Contact",
-        link: "/contact",
-    },
+	{
+		name: "Main",
+		link: "#",
+	},
+	{
+		name: "About",
+		link: "#",
+	},
+	{
+		name: "Services",
+		link: "#",
+	},
+	{
+		name: "Contact",
+		link: "#",
+	},
 ];
 
 const Navbar = () => {
@@ -29,7 +29,7 @@ const Navbar = () => {
 				<div className="container mx-auto h-full">
 					<nav className="text-white h-full flex items-center justify-between">
 						<div className="text-2xl">Logo</div>
-						<div className="z-10">
+						<div className="relative z-10">
 							<motion.button
 								animate={mobileNav ? "open" : "closed"}
 								onClick={() => toggleMobileNav()}
@@ -64,19 +64,67 @@ const Navbar = () => {
 							</motion.button>
 						</div>
 
-						<div className="fixed inset-0 w-full h-full overscroll-none overflow-hidden bg-blue-400/50">
-                            <div className="container">
-                                {NavItems.map((item) => (
-                                    <a
-                                        key={item.name}
-                                        href={item.link}
-                                        className="block py-4 text-2xl"
-                                    >
-                                        {item.name}
-                                    </a>
-                                ))}
-                            </div>
-                        </div>
+						<MotionConfig>
+							<AnimatePresence>
+								{mobileNav && (
+									<motion.div
+										key="fixed-container"
+										initial="closed"
+										animate="open"
+										exit="closed"
+										variants={{
+											closed: {
+												y: "-100%",
+												transition: {
+													delay: 0.2,
+													staggerChildren: 0.4,
+													ease: "easeInOut",
+												},
+											},
+											open: {
+												y: "0%",
+												transition: {
+													delayChildren: 0.4,
+													ease: "easeInOut",
+												},
+											},
+										}}
+										className="fixed inset-0 w-full h-full overscroll-none overflow-hidden bg-zinc-900"
+									>
+										<motion.div
+											variants={{
+												closed: {
+													opacity: 0,
+													y: "-1%",
+													transition: {
+														ease: "easeInOut",
+													},
+												},
+												open: {
+													opacity: 1,
+													y: "0%",
+													transition: {
+														ease: "easeInOut",
+													},
+												},
+											}}
+											key="link-container"
+											className="container py-16 flex flex-col gap-4"
+										>
+											{NavItems.map((item, id) => (
+												<a
+													key={id}
+													href={item.link}
+													className="block text-3xl font-medium"
+												>
+													{item.name}
+												</a>
+											))}
+										</motion.div>
+									</motion.div>
+								)}
+							</AnimatePresence>
+						</MotionConfig>
 					</nav>
 				</div>
 			</header>
