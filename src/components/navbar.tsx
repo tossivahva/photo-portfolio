3287c1ddf0dacb82aae8ddde923ae23cf7e899dd
tossivahva <tossivahva@gmail.com>
2024-05-10 14:36:16 +0300

    complete mobile nav

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
-import { AnimatePresence, motion, useCycle, MotionConfig } from "framer-motion";
+import { AnimatePresence, motion, useCycle } from "framer-motion";
+import { useEffect, useState } from "react";
 
 const NavItems = [
 	{
@@ -22,17 +23,34 @@ const NavItems = [
 
 const Navbar = () => {
 	const [mobileNav, toggleMobileNav] = useCycle(false, true);
+	const [isButtonDisabled, setButtonDisabled] = useState(false);
+
+	const handleClick = () => {
+		if (!isButtonDisabled) {
+			toggleMobileNav();
+			setButtonDisabled(true);
+			setTimeout(() => setButtonDisabled(false), 800);
+		}
+	};
+
+	useEffect(() => {
+		if (mobileNav) {
+			document.body.style.overflow = "hidden";
+		} else {
+			document.body.style.overflow = "unset";
+		}
+	}, [mobileNav]);
 
 	return (
 		<>
 			<header className="sticky top-0 w-full h-16 bg-zinc-800 inset-0">
 				<div className="container mx-auto h-full">
 					<nav className="text-white h-full flex items-center justify-between">
-						<div className="text-2xl">Logo</div>
-						<div className="relative z-10">
+						<motion.div className="text-2xl">Logo</motion.div>
+						<motion.div className="relative z-10">
 							<motion.button
 								animate={mobileNav ? "open" : "closed"}
-								onClick={() => toggleMobileNav()}
+								onClick={() => handleClick()}
 								className="flex flex-col space-y-1.5 -mr-5"
 							>
 								<motion.span
@@ -62,69 +80,73 @@ const Navbar = () => {
 									className="block w-6 h-0.5 bg-white rounded-full"
 								></motion.span>
 							</motion.button>
-						</div>
+						</motion.div>
 
-						<MotionConfig>
-							<AnimatePresence>
-								{mobileNav && (
+						<AnimatePresence mode="wait">
+							{mobileNav && (
+								<motion.div
+									key="fixed-container"
+									initial="closed"
+									animate="open"
+									exit="closed"
+									variants={{
+										closed: {
+											y: "-100%",
+											transition: {
+												delay: 0.15,
+												type: "spring",
+												bounce: 0,
+												ease: "easeInOut",
+											},
+										},
+										open: {
+											y: "0%",
+											transition: {
+												delayChildren: 0.3,
+												type: "spring",
+												bounce: 0,
+												ease: "easeInOut",
+											},
+										},
+									}}
+									className="fixed inset-0 w-full h-full overscroll-none overflow-hidden bg-zinc-900"
+								>
 									<motion.div
-										key="fixed-container"
-										initial="closed"
-										animate="open"
-										exit="closed"
+										transition={{
+											staggerChildren: 0.1,
+										}}
 										variants={{
 											closed: {
-												y: "-100%",
-												transition: {
-													delay: 0.2,
-													staggerChildren: 0.4,
-													ease: "easeInOut",
-												},
+												opacity: 0,
 											},
 											open: {
-												y: "0%",
-												transition: {
-													delayChildren: 0.4,
-													ease: "easeInOut",
-												},
+												opacity: 1,
 											},
 										}}
-										className="fixed inset-0 w-full h-full overscroll-none overflow-hidden bg-zinc-900"
+										key="link-container"
+										className="container py-16 flex flex-col gap-4"
 									>
-										<motion.div
-											variants={{
-												closed: {
-													opacity: 0,
-													y: "-1%",
-													transition: {
-														ease: "easeInOut",
+										{NavItems.map((item, id) => (
+											<motion.a
+												key={id}
+												href={item.link}
+												className="block text-3xl font-medium"
+												variants={{
+													closed: {
+														opacity: 0,
 													},
-												},
-												open: {
-													opacity: 1,
-													y: "0%",
-													transition: {
-														ease: "easeInOut",
+													open: {
+														opacity: 1,
 													},
-												},
-											}}
-											key="link-container"
-											className="container py-16 flex flex-col gap-4"
-										>
-											{NavItems.map((item, id) => (
-												<a
-													key={id}
-													href={item.link}
-													className="block text-3xl font-medium"
-												>
-													{item.name}
-												</a>
-											))}
-										</motion.div>
+												}}
+											>
+												{item.name}
+											</motion.a>
+										))}
 									</motion.div>
-								)}
-							</AnimatePresence>
-						</MotionConfig>
+								</motion.div>
+							)}
+						</AnimatePresence>
 					</nav>
 				</div>
 			</header>
