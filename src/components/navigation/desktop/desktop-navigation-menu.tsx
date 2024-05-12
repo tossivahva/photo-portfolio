568f50a09a6dd4389e1d2a965c154f6436628665
tossivahva <tossivahva@gmail.com>
2024-05-13 00:34:59 +0300

    finished with base radix ui float desktop navbar

--- src/components/navigation/desktop/desktop-navigation-menu.tsx
+++ src/components/navigation/desktop/desktop-navigation-menu.tsx
@@ -6,9 +6,9 @@ import { menuItems, collections } from '@/config/nav-items.ts';
 const DesktopNavigationMenu = () => {
     return (
         <NavigationMenu.Root className='relative z-[1] flex w-screen justify-center'>
-            <NavigationMenu.List className='center m-0 flex list-none rounded-full bg-white/10 backdrop-blur-2xl border border-white/10 p-1'>
+            <NavigationMenu.List className='center m-0 flex list-none rounded-full bg-black/60 backdrop-blur-2xl border border-white/10 p-2'>
                 <NavigationMenu.Item>
-                    <NavigationMenu.Trigger className='text-white hover:bg-black/30 group flex select-none items-center justify-between gap-[2px] rounded-full px-3 py-2 text-[15px] font-medium leading-none outline-none'>
+                    <NavigationMenu.Trigger className='text-white/70 hover:text-white group flex select-none items-center justify-between gap-[2px] rounded-full px-3 py-2 text-sm font-medium leading-none outline-none'>
                         Overview{' '}
                         <CaretDownIcon
                             className='text-white relative top-[1px] transition-transform duration-[250] ease-in group-data-[state=open]:-rotate-180'
@@ -16,11 +16,11 @@ const DesktopNavigationMenu = () => {
                         />
                     </NavigationMenu.Trigger>
                     <NavigationMenu.Content className='absolute top-0 left-0 w-full sm:w-auto'>
-                        <ul className='m-0 list-none gap-x-[10px] p-[22px] sm:w-[300px] sm:grid-flow-col'>
+                        <ul className='m-0 list-none gap-x-[10px] py-3 px-2 sm:w-[200px] sm:grid-flow-col'>
                             {collections.map((item) => (
                                 <li
                                     key={item.name}
-                                    className='text-violet11 hover:bg-violet3 focus:shadow-violet7 block select-none rounded-[4px] px-3 py-2 text-[15px] font-medium leading-none no-underline outline-none focus:shadow-[0_0_0_2px]'
+                                    className='text-white/70 hover:text-white block select-none rounded-lg px-3 py-2 text-sm font-medium leading-none no-underline outline-none'
                                 >
                                     {item.name}
                                 </li>
@@ -28,13 +28,25 @@ const DesktopNavigationMenu = () => {
                         </ul>
                     </NavigationMenu.Content>
                 </NavigationMenu.Item>
+                
+                {menuItems.map((item, id) => (
+                    <NavigationMenu.Item>
+                        <NavigationMenu.Link
+                            key={id}
+                            className='text-white/70 hover:text-white group flex select-none items-center justify-between gap-1 rounded-full px-3 py-2 text-sm font-medium leading-none outline-none'
+                        >
+                            {item.name}
+                        </NavigationMenu.Link>
+                    </NavigationMenu.Item>
+                ))}
+                
                 <NavigationMenu.Indicator className='data-[state=visible]:animate-fadeIn data-[state=hidden]:animate-fadeOut top-full z-[1] flex h-[10px] items-end justify-center overflow-hidden transition-[width,transform_250ms_ease]'>
-                    <div className='relative top-[70%] h-[10px] w-[10px] rotate-[45deg] rounded-tl-[2px] bg-white'/>
+                    <div className='relative top-[70%] h-[10px] w-[10px] rotate-[45deg] rounded-tl-[2px] bg-black/60 border border-white/10'/>
                 </NavigationMenu.Indicator>
             </NavigationMenu.List>
             
-            <div className='perspective-[2000px] absolute top-full left-0 flex w-full justify-center'>
-                <NavigationMenu.Viewport className='data-[state=open]:animate-scaleIn data-[state=closed]:animate-scaleOut relative mt-[10px] h-[var(--radix-navigation-menu-viewport-height)] w-full origin-[top_center] overflow-hidden rounded-[6px] bg-white transition-[width,_height] duration-300 sm:w-[var(--radix-navigation-menu-viewport-width)]'/>
+            <div className='perspective-[2000px] absolute top-full left-0 flex w-[200px] translate-x-[calc(50%+100px)] justify-center'>
+                <NavigationMenu.Viewport className='data-[state=open]:animate-scaleIn data-[state=closed]:animate-scaleOut relative mt-[10px] h-[var(--radix-navigation-menu-viewport-height)] w-full origin-[top_center] overflow-hidden rounded-xl bg-black/60 backdrop-blur-2xl border border-white/10 transition-[width,_height] duration-300 sm:w-[var(--radix-navigation-menu-viewport-width)]'/>
             </div>
         </NavigationMenu.Root>
     );
