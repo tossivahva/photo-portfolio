7d834171967b04774886e16e4465fed598632ff3
tossivahva <tossivahva@gmail.com>
2024-05-16 23:06:03 +0300

    fixed navbar z-lvling

--- src/components/navigation/desktop/desktop-navbar.tsx
+++ src/components/navigation/desktop/desktop-navbar.tsx
@@ -3,7 +3,7 @@ import { Button } from '@radix-ui/themes';
 
 export const DesktopNavbar = () => {
     return (
-        <nav className='fixed top-0 w-full h-16 inset-0 hidden lg:block'>
+        <nav className='fixed z-50 top-0 w-full h-16 inset-0 hidden lg:block'>
             <div className='container h-full flex justify-between items-center text-white'>
                 <DesktopNavigationMenu/>
             </div>
